96d2b2ee7fb0513807ca9d967dee4719b5aee32e
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2022-12-05 15:34:39 -0300

    feat: create key route and factort

--- app/presentation/factories/create_key_factory.py
+++ app/presentation/factories/create_key_factory.py
@@ -1,6 +1,10 @@
+from app.adapters.database import MongoDBAdapter
 from app.core.usecases.create_key import CreateKey, CreateKeyParams
 from app.ports.usecases import Usecase
 
 
 def create_key_factory(params: CreateKeyParams) -> Usecase:
-    return CreateKey(params)
+    return CreateKey(
+        params=params,
+        database=MongoDBAdapter()
+    )
--- app/presentation/routes/keys_routes.py
+++ app/presentation/routes/keys_routes.py
@@ -3,8 +3,10 @@ from http import HTTPStatus
 from fastapi import Request, Response
 
 from app.adapters.http import fastapi_adapter
+from app.core.helpers.http import HandledError
 from app.main import app
-from app.ports.usecases.create_key_port import CreateKeyParams
+from app.ports.usecases.create_key_port import (CreateKeyParams,
+                                                CreateKeyResponse)
 from app.ports.usecases.get_key_port import GetKeyParams
 from app.presentation.factories import create_key_factory, get_key_factory
 
@@ -14,12 +16,25 @@ PREFIX = '/keys'
 
 @app.post(
     PREFIX,
-    status_code=HTTPStatus.OK,
+    status_code=HTTPStatus.CREATED,
     summary='Criar chave PIX',
-    responses={},
+    responses={
+        HTTPStatus.CREATED.value: {
+            'model': CreateKeyResponse
+        },
+        HTTPStatus.BAD_REQUEST.value: {
+            'model': HandledError
+        },
+        HTTPStatus.NOT_FOUND.value: {
+            'model': HandledError
+        },
+        HTTPStatus.CONFLICT.value: {
+            'model': HandledError
+        }
+    },
     tags=TAGS
 )
-def create_key(request: Request, response: Response, body: CreateKeyParams):
+def create_key(response: Response, body: CreateKeyParams):
     return fastapi_adapter(response, create_key_factory(body))
 
 
