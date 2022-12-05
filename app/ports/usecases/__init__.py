16bfa80f18d1d2c09a39c848f0c501c29a3112bb
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2022-12-05 15:34:06 -0300

    feat: create key port

--- app/ports/usecases/__init__.py
+++ app/ports/usecases/__init__.py
@@ -1,8 +1,9 @@
 from .confirm_pix import ConfirmPixParams, ConfirmPixPort, ConfirmPixResponse
 from .create_institution_port import (CreateInstitutionParams,
-                                 CreateInstitutionPort,
-                                 CreateInstitutionResponse)
-from .create_key_port import CreateKeyParams, CreateKeyPort, CreateKeyResponse
+                                      CreateInstitutionPort,
+                                      CreateInstitutionResponse)
+from .create_key_port import (Account, CreateKeyParams, CreateKeyPort,
+                              CreateKeyResponse, Owner)
 from .get_key_port import GetKeyParams, GetKeyPort, GetKeyResponse
 from .get_pix import GetPixParams, GetPixPort, GetPixResponse
 from .refund_pix import RefundPixParams, RefundPixPort, RefundPixResponse
--- app/ports/usecases/create_key_port.py
+++ app/ports/usecases/create_key_port.py
@@ -1,17 +1,33 @@
 
 from abc import abstractmethod
 
+from app.core.enums.enums import AccountTypes, KeyTypes
 from app.core.helpers.http import HttpResponse
 
 from .usecase import NOT_IMPLEMENTED_ERROR, InputData, Usecase
 
 
+class Owner(InputData):
+    document: str
+    name: str
+
+
+class Account(InputData):
+    ispb: str
+    branch: str
+    account_number: str
+    account_type: AccountTypes
+
+
 class CreateKeyParams(InputData):
-    pass
+    key: str
+    key_type: KeyTypes
+    owner: Owner
+    account: Account
 
 
 class CreateKeyResponse(InputData):
-    pass
+    id: str
 
 
 class CreateKeyPort(Usecase):
