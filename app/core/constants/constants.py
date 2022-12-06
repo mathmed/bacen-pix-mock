ee6e98e287c310e149cf926aa7bbe1ec9e6cda4b
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2022-12-06 12:27:30 -0300

    refactor: constants

--- app/core/constants/constants.py
+++ app/core/constants/constants.py
@@ -1,2 +1,6 @@
 TOKEN_EXPIRES_IN = 3600
 ISS = 'Pix Mock - BACEN'
+INVALID_USER_PASS_OR_ISPB = 'Invalid user/password/ispb'
+ERROR_TO_AUTHENTICATE = 'Error to authenticate'
+ERROR_TO_SAVE = 'Error to save'
+NOT_IMPLEMENTED_ERROR = 'This contract method must be implemented'
