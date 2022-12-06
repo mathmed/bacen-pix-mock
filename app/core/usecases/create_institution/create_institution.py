d8bc8a7fd22bffa411a9b0aaf686e05548fb5b65
Mateus Medeiros <mateusmedeiros252525@hotmail.com>
2022-12-06 14:01:37 -0300

    refactor: messages

--- app/core/usecases/create_institution/create_institution.py
+++ app/core/usecases/create_institution/create_institution.py
@@ -4,7 +4,7 @@ from typing import Tuple
 from uuid import uuid4
 
 from app.core.collections import Auth, Institution
-from app.core.constants import ERROR_TO_SAVE
+from app.core.constants import ERROR_TO_SAVE_MESSAGE
 from app.core.helpers.http import HttpError, HttpResponse, HttpStatus
 from app.ports.external import DatabasePort, EncryptPort
 from app.ports.usecases import (CreateInstitutionParams, CreateInstitutionPort,
@@ -43,7 +43,7 @@ class CreateInstitution(CreateInstitutionPort):
         except Exception:
             print(format_exc())
             raise HttpError(HTTPStatus.BAD_REQUEST,
-                            ERROR_TO_SAVE)
+                            ERROR_TO_SAVE_MESSAGE)
 
     def _verify_if_ispb_exists(self):
         institutions = self.database.get_by_filters(
